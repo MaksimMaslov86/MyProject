d4fa5fa6fac0e929b82d50dd937bcfefb6efb13e
Maksim <maks_maslov@mail.ru>
2022-09-20 14:02:09 +0300

    added day 6 Task 1

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,22 +2,88 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.setCarModel("Форд");
-        car.setColor("желтый");
-        car.setYear(2020);
+        Car car1 = new Car();
+        car1.setModel("Honda");
+        car1.setColor("Brown");
+        car1.setYear(2021);
+        car1.info();
+        int difference1 = car1.yearDifference(2025);
+        System.out.println(difference1);
 
-        Bike bike = new Bike("Suzuki", "зеленый", 2021);
+        Bike bike1 = new Bike("Kawasaki", "Yellow", 2010);
+        bike1.info();
+        int difference2 = bike1.yearDifference(2022);
+        System.out.println(difference2);
+    }
+}
+class Car {
+    private String model;
+    private String color;
+    private int year;
 
-        car.info();
-        bike.info();
+    public void setModel(String model) {
+        this.model = model;
+    }
 
-        System.out.println("Марка автомобиля: " + car.getCarModel() + ", цвет: " + car.getColor() +
-                ", год выпуска: " + car.getYear());
-        System.out.println("Марка автомобиля: " + bike.getBikeModel() + ", цвет: " + bike.getColor() +
-                ", год выпуска: " + bike.getYear());
+    public String getModel() {
+        return model;
+    }
 
-        System.out.println(car.yearDifference(2020));
-        System.out.println(bike.yearDifference(2022));
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    void info() {
+        System.out.println("Это автомобиль");
+    }
+    int yearDifference (int inputYear) {
+        return Math.abs(year - inputYear);
+    }
+}
+class Bike {
+    private String model;
+    private String color;
+    private int year;
+
+    public Bike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setColor (String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return  color;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void info() {
+        System.out.println("Это мотоцикл");
+    }
+    int yearDifference (int inputYear) {
+        return Math.abs(year - inputYear);
     }
 }
