0107c570007effe3fe25722883b70fcf768db3dd
Maksim <maks_maslov@mail.ru>
2022-09-20 14:13:08 +0300

    added day 6 Task 2

--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -1,19 +1,27 @@
 package day6;
 
-class Airplane {
+public class Task2 {
+    public static void main(String[] args) {
+        Airplain plain1 = new Airplain("Boieng", 2020, 36.5, 10000);
+        plain1.setYear(2021);
+        plain1.setLength(40);
+        plain1.fillUp(10);
+        plain1.fillUp(5);
+        plain1.info();
+    }
+}
+class Airplain {
     private String manufacturer;
     private int year;
     private double length;
     private double weight;
-    private double fuel = 0;
-
-    Airplane(String manufacturer, int year, double length, double weight) {
+    private double fuel;
+    public Airplain(String manufacturer, int year, double length, double weight) {
         this.manufacturer = manufacturer;
         this.year = year;
         this.length = length;
         this.weight = weight;
     }
-
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
@@ -26,34 +34,17 @@ class Airplane {
     public void setWeight(double weight) {
         this.weight = weight;
     }
-    public void setFuel(double fuel) {
+    public void setFuel (double fuel) {
         this.fuel = fuel;
     }
-    public double getFuel(){
+    public double getFuel() {
         return fuel;
     }
-
-    void info() {
-        System.out.println("Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " +
-                           length + ", вес " + weight + ", количество топлива в баке: " + fuel +
-                           " литров");
+    public void info() {
+        System.out.println("Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length
+                           + ", вес: " + weight + ", количество топлива в баке: " + fuel);
     }
-    public double fillUp(int n) {
+    public void fillUp(int n) {
         fuel += n;
-        return fuel;
-    }
-
-}
-
-public class Task2 {
-    public static void main(String[] args) {
-        Airplane plane = new Airplane("Boeing", 2022, 36.5, 15000);
-
-        plane.setYear(2008);
-        plane.setLength(38);
-        plane.fillUp(135);
-        plane.fillUp(326);
-        plane.info();
-
     }
 }
