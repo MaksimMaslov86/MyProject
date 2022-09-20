a3d5bfbb5c009840e7c5b569f1424e18c4f01bf1
Maksim <maks_maslov@mail.ru>
2022-09-20 13:45:23 +0300

    added day 4 Task 1

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,43 +1,38 @@
 package day4;
-import java.util.Scanner;
-import java.util.Random;
+
+import java.util.*;
 
 public class Task1 {
     public static void main(String[] args) {
+        Random rand = new Random();
         Scanner sc = new Scanner(System.in);
-        Random ran = new Random();
-        int x = sc.nextInt();
-        int[] numbers = new int[x];
-        int sum = 0;
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        int countLager8 = 0;
+        int count1 = 0;
         int countEvenNum = 0;
         int countOddNum = 0;
-        int countOne = 0;
-        int countEight = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = ran.nextInt(10) + 1;
-        }
-        for (int number:numbers) {
-            System.out.println(number);
-            sum += number;
-            if (number > 8) {
-                countEight++;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            array[i] = rand.nextInt(11);
+            sum += array[i];
+            if (array[i] > 8) {
+                countLager8++;
             }
-            if (number == 1) {
-                countOne++;
+            if (array[i] == 1) {
+                count1++;
             }
-            if (number % 2 != 0) {
+            if (array[i] % 2 == 0) {
+                countEvenNum++;
+            } else {
                 countOddNum++;
             }
-            if (number % 2 == 0) {
-                countEvenNum++;
-            }
         }
-
-        System.out.println("Длина массива: " + numbers.length);
-        System.out.println("Количество чисел больше 8: " + countEight);
-        System.out.println("Количество чисел равных 1: " + countOne);
+        System.out.println("Длина массива: " + array.length);
+        System.out.println("Количество чисел больше 8: " + countLager8);
+        System.out.println("Количество чисел больше 1: " + count1);
         System.out.println("Количество четных чисел: " + countEvenNum);
-        System.out.println("Количество нечетных числе: " + countOddNum);
-        System.out.println("Сумма всех чисел массива: " + sum);
+        System.out.println("Количество нечетных чисел: " + countOddNum);
+        System.out.println("Количество четных чисел: " + sum);
     }
 }
