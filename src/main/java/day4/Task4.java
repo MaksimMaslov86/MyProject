726e347e1cbd8624f11b3985f7a3cecbddb6b01e
Maksim <maks_maslov@mail.ru>
2022-09-20 14:56:24 +0300

    added day 4 Task 4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -4,22 +4,22 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         Random ran = new Random();
-        int[] numbers = new int[100];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = ran.nextInt(10000);
+        int[] array = new int[100];
+        int index = 0;
+        int sum3 = 0;
+        int maxSum3 = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ran.nextInt(10000);
         }
-        int maxSum = 0;
-        int sum;
-        int count = 0;
-        for (int j = 2; j < numbers.length; j++) {
-            sum = numbers[j] + numbers[j-1] + numbers[j-2];
-            if (sum >= maxSum) {
-                maxSum = sum;
-                count = j-2;
+        for (int j = 0; j < array.length-2; j++) {
+            sum3 = array[j] + array[j+1] + array[j+2];
+            if (sum3 > maxSum3) {
+                maxSum3 = sum3;
+                index = j;
             }
-
+            sum3 = 0;
         }
-        System.out.println(maxSum);
-        System.out.println(count);
+        System.out.println(maxSum3 + ", " + index);
+
     }
 }
